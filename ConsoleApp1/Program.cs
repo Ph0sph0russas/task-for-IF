9b8382acff27bc8806f996122d80701fa4efa6ec
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-02-19 19:15:41 +0800

    Скорректирован код. Взаимодействие с пользователем отделено от логики.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,42 +7,42 @@ namespace Task1
         static void Main(string[] args)
         {
             int costOfProduct = int.Parse(Console.ReadLine());
+
+
             int rubles = costOfProduct / 100;
             int ruPenny = costOfProduct % 100;
+            string messageAnswer = "";
             if (rubles == 1 || rubles % 10 == 1)
             {
-                Console.Write(rubles);
-                Console.Write(" рубль ");
+                messageAnswer = rubles + " рубль ";
             }
             else if (((rubles >= 10 && rubles <= 20) || ((rubles % 10) >= 5 && (rubles % 10) <= 9) || (rubles % 10) == 0) && rubles != 0)
             {
-                Console.Write(rubles);
-                Console.Write(" рублей ");
+                messageAnswer = rubles + " рублей ";
             }
             else if ((rubles % 10) > 1 && (rubles % 10) < 5)
             {
-                Console.Write(rubles);
-                Console.Write(" рубля ");
+                messageAnswer = rubles + " рубля ";
             }
             if (ruPenny == 0 && rubles != 0)
             {
-                Console.Write("ровно");
+                messageAnswer = messageAnswer + "ровно";
             }
             else if (ruPenny == 1 || ruPenny % 10 == 1)
             {
-                Console.Write(ruPenny);
-                Console.Write(" копейка");
+                messageAnswer = messageAnswer + ruPenny + " копейка";
             }
             else if ((ruPenny >= 10 && ruPenny <= 20) || ((ruPenny % 10) >= 5 && (ruPenny % 10) <= 9) || (ruPenny % 10) == 0)
             {
-                Console.Write(ruPenny);
-                Console.Write(" копеек");
+                messageAnswer = messageAnswer + ruPenny + " копеек";
             }
             else if ((ruPenny % 10) > 1 && (ruPenny % 10) < 5)
             {
-                Console.Write(ruPenny);
-                Console.Write(" копейки");
+                messageAnswer = messageAnswer + ruPenny + " копейки";
+
             }
+
+            Console.WriteLine(messageAnswer);
         }
     }
 }
