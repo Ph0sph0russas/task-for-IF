60ec2f9065d6317e8bd9f48d079354eee10e85c5
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-02-19 19:01:34 +0800

    Первая версия программы, не включает в себя проверки, тесты и декомпозицию

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int costOfProduct = int.Parse(Console.ReadLine());
+            int rubles = costOfProduct / 100;
+            int ruPenny = costOfProduct % 100;
+            if (rubles == 1)
+            {
+                Console.WriteLine(rubles);
+                Console.WriteLine(" рубль ");
+            }
+            else if ((rubles >= 10 && rubles <= 20) || ((rubles % 10) >= 5 && (rubles % 10) <= 9) || (rubles % 10) == 0)
+            {
+                Console.WriteLine(rubles);
+                Console.WriteLine(" рублей ");
+            }
+            else if ((rubles % 10) > 1 && (rubles % 10) < 5)
+            {
+                Console.WriteLine(rubles);
+                Console.WriteLine(" рубля ");
+            }
+            if (ruPenny == 0)
+            {
+                Console.WriteLine("ровно");
+            }
+            else if (ruPenny == 1)
+            {
+                Console.WriteLine(ruPenny);
+                Console.WriteLine("копейка");
+            }
+            else if ((ruPenny >= 10 && ruPenny <= 20) || ((ruPenny % 10) >= 5 && (ruPenny % 10) <= 9) || (ruPenny % 10) == 0)
+            {
+                Console.WriteLine(ruPenny);
+                Console.WriteLine(" копеек");
+            }
+            else if ((ruPenny % 10) > 1 && (ruPenny % 10) < 5)
+            {
+                Console.WriteLine(ruPenny);
+                Console.WriteLine(" копейки");
+            }
+        }
+    }
+}
